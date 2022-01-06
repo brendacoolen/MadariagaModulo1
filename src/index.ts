ed9e5f039cce04b71ba5eb7ff0899680d1f6d365
brendacoolen <92064251+brendacoolen@users.noreply.github.com>
2022-01-06 20:11:02 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+console.log("Supermercado Hernan");
+console.log("fideos soperos Arcor", "100 unidades");
+console.log("fideos spaghetti Terrabusi", "150 unidades");
+console.log("pure de tomates Mora", "200 unidades");
+console.log("jugo Tang", "80 unidades");
+console.log("galletitas Surtido Bagley", "300 unidades");
+console.log("desodorante Axe", "180 unidades");
+console.log("miel La Reina", "220 unidades");
+console.log("arroz Gallo", "300 unidades");
+console.log("lentejas Abadia", "140 unidades");
